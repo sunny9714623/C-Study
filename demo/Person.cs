30d7048a1920be398108a1fd87e1531ce9143d6a
sunny9714623 <971462342@qq.com>
2021-10-20 17:24:32 +0800

    Chapter05 done

---
+++ demo/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Packt.Shared
+{
+    public partial class Person
+    {
+        public string Name;
+        public string HomePlanet="Earth";
+        public List<Person> Children;
+        public DateTime DateOfBirth;
+    }
+}
--- demo/Program.cs
+++ demo/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Packt.Shared;
+using static System.Console;
 
 namespace demo
 {
@@ -6,7 +8,26 @@ namespace demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var sam = new Person()
+            {
+                Name = "Sam",
+                HomePlanet = "Planet",
+                DateOfBirth = new DateTime(1972, 1, 27)
+            };
+            sam.FavoriteIceCream = "Chocolate Fudge";
+            WriteLine($"Sam's favorite ice-cream flavor is {sam.FavoriteIceCream}");
+            sam.FavoritePrimaryColor = "Red";
+            WriteLine($"Sam's favorite primary color is {sam.FavoritePrimaryColor}.");
+            sam.Children = new System.Collections.Generic.List<Person>();
+            sam.Children.Add(new Person { Name = "Charlie" });
+            sam.Children.Add(new Person { Name = "Ella" });
+            WriteLine($"Sam's first child is {sam.Children[0].Name}");
+            WriteLine($"Sam's second child is {sam.Children[1].Name}");
+            WriteLine($"Sam's first child is {sam[0].Name}");
+            WriteLine($"Sam's second child is {sam[1].Name}");
+            WriteLine(sam.Origin);
+            WriteLine(sam.Greeting);
+            WriteLine(sam.Age);
         }
     }
 }
