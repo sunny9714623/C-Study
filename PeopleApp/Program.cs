9757f848bcc59b74fe54edb3857770fc25c2e91e
sunny9714623 <971462342@qq.com>
2021-10-20 18:40:22 +0800

    Chapter06

---
+++ PeopleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using Packt.Shared;
+using System;
+using static System.Console;
+
+namespace PeopleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var harry = new Person { Name = "Harry" };
+            var mary = new Person { Name = "Mary" };
+            var jill = new Person { Name = "Jill" };
+
+            var baby1 = mary.ProcreateWith(harry);
+            var baby2 = Person.Procreate(harry, jill);
+            var baby3 = harry * mary;
+            WriteLine($"{harry.Name} has {harry.Children.Count} children.");
+            WriteLine($"{mary.Name} has {mary.Children.Count} children.");
+            WriteLine($"{jill.Name} has {jill.Children.Count} children.");
+            WriteLine(format: "{0}'s first child is named \"{1}\". ", arg0: harry.Name, arg1: harry.Children[0].Name);
+        }
+    }
+}
