febcbb8b2d1712430450bb34cfb83b258ca8eb51
sunny9714623 <971462342@qq.com>
2021-10-29 18:36:03 +0800

    update

---
+++ WorkingWithFileSystems/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+using static System.IO.Directory;
+using static System.IO.Path;
+using static System.Environment;
+
+namespace WorkingWithFileSystems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //OutputFileSystemInfo();
+            WorkWithDrives();
+        }
+        static void OutputFileSystemInfo()
+        {
+            WriteLine("{0,-33} {1}", "Path.PathSeparator", PathSeparator);
+            WriteLine("{0,-33} {1}", "Path.DirectorySeparatorChar", DirectorySeparatorChar);
+
+            WriteLine("{0,-33} {1}", "Directory.GetCurrentDirectory()", GetCurrentDirectory());
+            WriteLine("{0,-33} {1}", "Environment.CurrentDirectory", CurrentDirectory);
+            WriteLine("{0,-33} {1}", "Environment.SystemDirectory", SystemDirectory);
+            WriteLine("{0,-33} {1}", "Path.GetTempPath()", GetTempPath());
+
+            WriteLine("GetFolderPath(SpecialFolder)");
+            WriteLine("{0,-33} {1}", " .System", GetFolderPath(SpecialFolder.System));
+            WriteLine("{0,-33} {1}", " .ApplicationData", GetFolderPath(SpecialFolder.ApplicationData));
+            WriteLine("{0,-33} {1}", " .MyDocuments", GetFolderPath(SpecialFolder.MyDocuments));
+            WriteLine("{0,-33} {1}", " .Personal", GetFolderPath(SpecialFolder.Personal));
+        }
+
+        static void WorkWithDrives()
+        {
+            WriteLine("{0,-30} | {1,-10} | {2,-7} | {3,18} | {4,18}", "NAME", "TYPE", "FORMAT", "SIZE (BYTES)", "FREE SPACE");
+            foreach(DriveInfo drive in DriveInfo.GetDrives())
+            {
+                if (drive.IsReady)
+                {
+                    WriteLine("{0,-30} | {1,10} |{2,-7} | {3,19:N0} | {4,18:N0}", drive.Name, drive.DriveType, drive.DriveFormat, drive.TotalSize, drive.AvailableFreeSpace);
+                }
+            }
+        }
+    }
+}
--- WorkingWithReflection/Program.cs
+++ WorkingWithReflection/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Reflection;
 using static System.Console;
+using System.Linq;
 
 namespace WorkingWithReflection
 {
@@ -10,20 +11,38 @@ namespace WorkingWithReflection
         {
             WriteLine("Assembly metadata:");
             Assembly assembly = Assembly.GetEntryAssembly();
-            WriteLine($" Full name: {assembly.FullName}");
-            WriteLine($" Location: {assembly.Location}");
+            //WriteLine($" Full name: {assembly.FullName}");
+            //WriteLine($" Location: {assembly.Location}");
 
-            var attributes = assembly.GetCustomAttributes();
-            WriteLine($" Attributes");
-            foreach(Attribute a in attributes)
+            //var attributes = assembly.GetCustomAttributes();
+            //WriteLine($" Attributes");
+            //foreach(Attribute a in attributes)
+            //{
+            //    WriteLine($" {a.GetType()}");
+            //}
+
+            //var version = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+            //WriteLine($" Version : {version.InformationalVersion}");
+            //var company = assembly.GetCustomAttribute<AssemblyCompanyAttribute>();
+            //WriteLine($" Company : {company.Company}");
+            WriteLine();
+            WriteLine($"* Types:");
+            Type[] types = assembly.GetTypes();
+            foreach (Type type in types)
             {
-                WriteLine($" {a.GetType()}");
+                WriteLine();
+                WriteLine($"Type : {type.FullName}");
+                MemberInfo[] members = type.GetMembers();
+                foreach(MemberInfo member in members)
+                {
+                    WriteLine("{0}:{1}({2})", member.MemberType, member.Name, member.DeclaringType.Name);
+                    var coders = member.GetCustomAttributes<CoderAttribute>().OrderByDescending(c => c.LastModified);
+                    foreach(CoderAttribute coder in coders)
+                    {
+                        WriteLine("-> Modified by {0} on {1}", coder.Coder, coder.LastModified.ToShortDateString());
+                    }
+                }
             }
-
-            var version = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
-            WriteLine($" Version : {version.InformationalVersion}");
-            var company = assembly.GetCustomAttribute<AssemblyCompanyAttribute>();
-            WriteLine($" Company : {company.Company}");
         }
         [Coder("Mark Price","22 August 2019")]
         [Coder("Johnni Rasmussen","13 September 2019")]
