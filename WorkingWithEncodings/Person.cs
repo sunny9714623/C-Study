4792a3378877216ede0ded87e3ec83abf8722001
sunny9714623 <971462342@qq.com>
2021-11-02 18:48:03 +0800

    update

---
+++ WorkingWithEncodings/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Packt.Shared
+{
+    public class Person
+    {
+        public Person(decimal inintialSalary)
+        {
+            Salary = inintialSalary;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public HashSet<Person> Children { get; set; }
+        protected decimal Salary { get; set; }
+    }
+}
---
+++ WorkingWithSerialization/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Packt.Shared
+{
+    public class Person
+    {
+        public Person(decimal inintialSalary)
+        {
+            Salary = inintialSalary;
+        }
+        public Person() { }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public HashSet<Person> Children { get; set; }
+        protected decimal Salary { get; set; }
+    }
+}
