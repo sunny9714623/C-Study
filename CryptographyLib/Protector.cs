17403d0076bad2ea08a5076bd6e42a40a730aba5
George <971462342@qq.com>
2021-11-04 00:35:09 +0800

    update

--- CryptographyLib/Protector.cs
+++ CryptographyLib/Protector.cs
@@ -13,5 +13,49 @@ namespace Packt.Shared
     {
         // salt size must be at least 8 bytes,we will use 16 bytes
         private static readonly byte[] salt = Encoding.Unicode.GetBytes("7BANANAS");
+
+        // iterations must be at least 1000, we will use 2000
+        private static readonly int iterations = 200;
+        public static string Encrypt(
+            string plainText,string password)
+        {
+            byte[] encryptedBytes;
+            byte[] plainBytes = Encoding.Unicode.GetBytes(plainText);
+            var aes = Aes.Create();
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
+            aes.Key = pbkdf2.GetBytes(32); // set a 256-bit key
+            aes.IV = pbkdf2.GetBytes(16); // set a 128-bit key
+            using (var ms = new MemoryStream())
+            {
+                using(var cs=new CryptoStream(
+                    ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(plainBytes, 0, plainBytes.Length);
+                }
+                encryptedBytes = ms.ToArray();
+            }
+            return Convert.ToBase64String(encryptedBytes);
+        }
+
+        public static string Decrypt(
+            string cryptoText,string password)
+        {
+            byte[] plainBytes;
+            byte[] cryptoBytes = Convert.FromBase64String(cryptoText);
+            var aes = Aes.Create();
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
+            aes.Key = pbkdf2.GetBytes(32);
+            aes.IV = pbkdf2.GetBytes(16);
+            using(var ms=new MemoryStream())
+            {
+                using(var cs=new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(cryptoBytes, 0, cryptoBytes.Length);
+                }
+                plainBytes = ms.ToArray();
+            }
+
+            return Encoding.Unicode.GetString(plainBytes);
+        }
     }
 }
