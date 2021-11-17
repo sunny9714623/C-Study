5d06b189495773f9161067be5a2a817c2a67b64a
sunny9714623 <971462342@qq.com>
2021-11-17 18:54:32 +0800

    update

---
+++ LinqWithArrayOfExceptions/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using static System.Console;
+
+namespace LinqWithArrayOfExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinqWithArrayOfExceptions();
+        }
+        static void LinqWithArrayOfExceptions()
+        {
+            var errors = new Exception[]
+            {
+                new ArgumentException(),
+                new SystemException(),
+                new IndexOutOfRangeException(),
+                new InvalidOperationException(),
+                new NullReferenceException(),
+                new InvalidCastException(),
+                new OverflowException(),
+                new DivideByZeroException(),
+                new ApplicationException()
+            };
+            var numberErrors = errors.OfType<ArithmeticException>();
+            foreach(var error in numberErrors)
+            {
+                WriteLine(error);
+            }
+        }
+    }
+}
--- LinqWithArrayOfStrings/Program.cs
+++ LinqWithArrayOfStrings/Program.cs
@@ -15,7 +15,7 @@ namespace LinqWithArrayOfStrings
         {
             var names = new string[] { "Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin", "Toby", "Creed" };
             //var query = names.Where(new Func<string, bool>(NameLongerThanFour));
-            var query = names.Where(name => name.Length > 4);
+            var query = names.Where(name => name.Length > 4).OrderBy(name => name.Length).ThenBy(t => t);
             foreach (string item in query)
             {
                 WriteLine(item);
