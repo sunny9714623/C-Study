81864b3ddb65a1cae56bce00b274c5248a7caaf9
sunny9714623 <971462342@qq.com>
2021-11-22 18:07:33 +0800

    2021/11/22's work

---
+++ AsyncConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace AsyncConsole
+{
+    class Program
+    {
+        // 如果有以Async方法结束的方法那么应该使用这个方法而不是不以Async作为后缀的方法。
+        // 使用await关键字调用这个方法，使用async关键字进行修饰
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            HttpResponseMessage responseMessage = await client.GetAsync("http://www.apple.com/");
+            WriteLine("Apple's home page has {0:N0} bytes.", responseMessage.Content.Headers.ContentLength);
+        }
+    }
+}
---
+++ SynchronizingResourceAccess/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using static System.Console;
+
+namespace SynchronizingResourceAccess
+{
+    class Program
+    {
+        static Random r = new Random();
+        static string Message;// a shared resource
+        static object conch = new object();
+
+        static int Counter;// another shared resource.
+
+        static void MethodA()
+        {
+            // 容易死锁，需要自己写try，catch，使用minitor进行处理
+            //lock (conch)
+            //{
+            //    for (int i = 0; i < 5; i++)
+            //    {
+            //        Thread.Sleep(r.Next(2000));
+            //        Message += "A";
+            //        Write(".");
+            //    }
+            //}
+            try
+            {
+                Monitor.TryEnter(conch, TimeSpan.FromSeconds(15));
+                for (int i = 0; i < 5; i++)
+                {
+                    Thread.Sleep(r.Next(2000));
+                    Message += "A";
+                    Interlocked.Increment(ref Counter);
+                    Write(".");
+                }
+            }
+            finally
+            {
+                Monitor.Exit(conch);
+            }
+        }
+
+        static void MethodB()
+        {
+            //lock (conch)
+            //{
+            //    for (int i = 0; i < 5; i++)
+            //    {
+            //        Thread.Sleep(r.Next(2000));
+            //        Message += "B";
+            //        Write(".");
+            //    }
+            //}
+            try
+            {
+                Monitor.TryEnter(conch, TimeSpan.FromSeconds(15));
+                for (int i = 0; i < 5; i++)
+                {
+                    Thread.Sleep(r.Next(2000));
+                    Message += "B";
+                    Interlocked.Increment(ref Counter);
+                    Write(".");
+                }
+            }
+            finally
+            {
+                Monitor.Exit(conch);
+            }
+        }
+        static void Main(string[] args)
+        {
+            WriteLine("Please wait for the tasks to complete.");
+            Stopwatch watch = Stopwatch.StartNew();
+            Task a = Task.Factory.StartNew(MethodA);
+            Task b = Task.Factory.StartNew(MethodB);
+
+            Task.WaitAll(new Task[] { a, b });
+            WriteLine();
+            WriteLine($"Results :{Message}");
+            WriteLine($"{watch.ElapsedMilliseconds:#,##0} elapsed milliseconds.");
+
+            WriteLine($"{Counter} string Modifications.");
+        }
+
+    }
+}
