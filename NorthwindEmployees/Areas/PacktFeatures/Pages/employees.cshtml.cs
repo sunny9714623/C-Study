3c73d04a880d8aa233d669f5652fabd4aa492888
George <971462342@qq.com>
2021-11-24 23:11:06 +0800

    11/24 's work

---
+++ NorthwindEmployees/Areas/PacktFeatures/Pages/employees.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Packt.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PacktFeature.Pages
+{
+    public class EmployeesPageModel : PageModel
+    {
+        private Northwind db;
+        public EmployeesPageModel(Northwind injectedContext)
+        {
+            db = injectedContext;
+        }
+        public IEnumerable<Employee> Employees { get; set; }
+        public void OnGet()
+        {
+            Employees = db.Employees.ToArray();
+        }
+    }
+}
