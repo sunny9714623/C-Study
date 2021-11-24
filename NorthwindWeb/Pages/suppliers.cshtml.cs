f1ea53802985873777d7c278bc56e32e1e300b5d
GeorgeL <971462342@qq.com>
2021-11-24 18:32:30 +0800

    update

--- NorthwindWeb/Pages/suppliers.cshtml.cs
+++ NorthwindWeb/Pages/suppliers.cshtml.cs
@@ -1,10 +1,9 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Linq;
 using Packt.Shared;
 
 namespace NorthwindWeb.Pages
@@ -12,6 +11,9 @@ namespace NorthwindWeb.Pages
     public class SuppliersModel : PageModel
     {
         public IEnumerable<string> Suppliers { get; set; }
+        // 自动绑定属性
+        [BindProperty]
+        public Supplier Supplier { get; set; }
         private Northwind db;
         public SuppliersModel(Northwind injectedContext)
         {
@@ -21,6 +23,17 @@ namespace NorthwindWeb.Pages
         {
             ViewData["Title"] = "Northwind Web Site - Suppliers";
             Suppliers = db.Suppliers.Select(s => s.CompanyName);
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                db.Suppliers.Add(Supplier);
+                db.SaveChanges();
+                return RedirectToPage("/suppliers");
+            }
+            return Page();
         }
     }
 }
