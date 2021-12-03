979e9ee29143905c7b88deb4986282cce94511ab
George <971462342@qq.com>
2021-12-03 21:00:08 +0800

    update

--- BasicWinForms/Program.cs
+++ BasicWinForms/Program.cs
@@ -17,7 +17,7 @@ namespace BasicWinForms
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LegacyWinForms.Form1());
+            Application.Run(new LegacyWinForms.Form1() );
         }
     }
 }
