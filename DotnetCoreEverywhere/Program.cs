7103aa4415bc9badaa8989940be3a1679541925a
George <971462342@qq.com>
2021-12-08 22:46:56 +0800

    test

--- DotnetCoreEverywhere/Program.cs
+++ DotnetCoreEverywhere/Program.cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using static System.Console;
 
 namespace DotnetCoreEverywhere
 {
     class Program
     {
+        // class类型返回的是引用类型，这样可以直接更改。
         static void Main(string[] args)
         {
-            WriteLine("I can run everywhere!");
+            List<Student> abc = new List<Student>() { new Student { ID = 5, Name = "George" }, new Student { ID = 7, Name = "Simon" } };
+            var a = abc.Where(t => t.ID==5).FirstOrDefault();
+            a.Name = "123";
+            foreach (var item in abc)
+            {
+                Console.WriteLine(item.Name);
+            }
         }
     }
 }
